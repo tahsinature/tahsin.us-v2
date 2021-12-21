18fb9baa6653c3c3f57038e6ddeebabf2f5e518b
Tahsin <tahsingenuies@gmail.com>
2021-12-21 22:41:21 +0700

    allow all headers

--- server/pkg/middlewares/cors.go
+++ server/pkg/middlewares/cors.go
@@ -8,7 +8,7 @@ func Cors(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 	c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
