294ba2bfd9d7f838dc4b848ba372be9db2d17a60
Tahsin <tahsingenuies@gmail.com>
2021-12-23 10:21:09 +0700

    update description

--- client/src/components/HomeSections/Sections/FewWordsAboutMe/FewWordsAboutMe.tsx
+++ client/src/components/HomeSections/Sections/FewWordsAboutMe/FewWordsAboutMe.tsx
@@ -8,9 +8,9 @@ const FewWordsAboutMe = () => {
     <Section>
       <Header>Few words about myself</Header>
       <p style={{ textAlign: 'justify' }}>
-        I'm a Jakarta based Software Engineer. A passionate and pragmatic programmer with {moment.duration(moment().diff(moment().set('year', 2018).startOf('year'))).humanize()} of professional
-        experience, specializing in microservices & full-stack development using modern & robust technologies. Sometimes I work on things I find interesting, or things I think other people might find
-        interesting. It’s nice when those things overlap.
+        I am a solutions-oriented person who enjoys being challenged and engaged with projects that require me to work outside my comfort and knowledge set & a passionate and pragmatic programmer with{' '}
+        {moment.duration(moment().diff(moment().set('year', 2018).startOf('year'))).humanize()} of professional experience, specializing in microservices & full-stack development using modern & robust
+        technologies. Sometimes I work on things I find interesting, or things I think other people might find interesting. It’s nice when those things overlap.
       </p>
     </Section>
   );
