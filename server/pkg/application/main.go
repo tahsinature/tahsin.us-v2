ed4fc42febf587922ef34e060ee164557ff8cc82
Tahsin <tahsingenuies@gmail.com>
2021-12-31 16:42:24 +0700

    init telegram service on app start

--- server/pkg/application/main.go
+++ server/pkg/application/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/tahsinature/future-proof-gin/pkg/config"
+	"github.com/tahsinature/future-proof-gin/pkg/services"
 )
 
 type Application struct{}
@@ -12,6 +13,7 @@ type Application struct{}
 func (*Application) Setup() {
 	config.Validate()
 	gin.SetMode(gin.ReleaseMode)
+	new(services.Telegram).Init()
 
 	// db.Init()
 	// db.InitRedis()
