df086a157b0474535b3ed0c726e1e527a4e37371
Tahsin <tahsingenuies@gmail.com>
2022-01-01 13:31:19 +0700

    add ip service

---
+++ server/pkg/services/ip.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type (
+	IP       struct{}
+	Response struct {
+		Ip          string `json:"ip"`
+		City        string `json:"city"`
+		Region      string `json:"region"`
+		CountryName string `json:"country_name"`
+		ASN         string `json:"asn"`
+		Org         string `json:"org"`
+		Error       bool   `json:"error"`
+		Reason      string `json:"reason"`
+		Reserved    bool   `json:"reserved"`
+		Version     string `json:"version"`
+	}
+)
+
+func (IP) Lookup(ip string) Response {
+	client := resty.New()
+	url := fmt.Sprintf("https://ipapi.co/%s/json", ip)
+	response, err := client.R().Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	var resp Response
+	json.Unmarshal(response.Body(), &resp)
+
+	return resp
+}
