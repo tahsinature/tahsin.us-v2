7e2a1e2fbda8a085ba5c42324815be67e235d9e2
Tahsin <tahsingenuies@gmail.com>
2022-01-13 21:52:25 +0700

    add static images

---
+++ client/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import nikonImg from './tahsin-nikon-z50.jpg';
+// import nikonImg from './music-concert.jpg';
+
+const images = {
+  otherActivities: {
+    photography: nikonImg,
+    // photography: nikonImg,
+  },
+};
+
+export default images;
