3afe931e9105e32233c9197e199f87bc3eb6e4cf
Tahsin <tahsingenuies@gmail.com>
2022-01-13 22:03:56 +0700

    use dynamic central images

--- client/src/components/HomeSections/Sections/OtherActivities/OtherActivities.tsx
+++ client/src/components/HomeSections/Sections/OtherActivities/OtherActivities.tsx
@@ -6,33 +6,34 @@ import { RowingOutlined } from '@material-ui/icons';
 import classes from './OtherActivities.module.scss';
 import Header from 'src/components/Header/Header';
 import Section from 'src/components/Section/Section';
+import images from 'src/assets/images';
 
 let history: History;
 
 const activities = [
   {
     name: 'Books',
-    image: 'https://thecopybot.com/wp-content/uploads/2011/07/book-business-coffee-324129-1024x683.jpg',
+    image: images.otherActivities.books,
     clickHandler: () => alert('I will add soon.'),
   },
   {
     name: 'Photography',
-    image: 'https://gallery.tahsin.us/uploads/big/273cfc916035a077d9612fbb3258acb7.jpeg',
+    image: images.otherActivities.photography,
     clickHandler: () => history.push('/gallery'),
   },
   {
     name: 'Movies & TV Series',
-    image: 'https://media.istockphoto.com/photos/popcorn-and-clapperboard-picture-id1191001701?k=20&m=1191001701&s=612x612&w=0&h=uDszifNzvgeY5QrPwWvocFOUCw8ugViuw-U8LCJ1wu8=',
+    image: images.otherActivities.movies,
     clickHandler: () => alert('I will add soon.'),
   },
   {
     name: 'Music',
-    image: 'https://img.static-kl.com/images/media/E95E1F32-A7A4-4FC0-AC0A41A2BC528AE3?aspect_ratio=1:1&min_width=912',
+    image: images.otherActivities.music,
     clickHandler: () => alert('I will add soon.'),
   },
   {
     name: 'Travel',
-    image: 'https://media-exp1.licdn.com/dms/image/C4D1BAQGGdNo6IlDOCQ/company-background_10000/0/1519801807380?e=2159024400&v=beta&t=VWHuXHsmYAXZB4XQn7H63FLLvHjjMfs6CdxCh3HwcCM',
+    image: images.otherActivities.travel,
     clickHandler: () => history.push('/travels'),
   },
 ];
