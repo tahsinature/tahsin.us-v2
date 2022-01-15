00430ab5d3aaf4b5017fcdbcf9534658b04478e0
Tahsin <tahsingenuies@gmail.com>
2022-01-16 05:28:15 +0700

    put index name

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import HomeSections from 'src/components/HomeSections/HomeSections';
-import data from 'src/api/data';
+import data from 'src/api/data/index';
 import classes from './Home.module.scss';
 
 const Home = () => {
