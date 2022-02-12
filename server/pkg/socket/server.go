f613f1823f03b7b65571a355c084282a05de90b3
Tahsin <tahsingenuies@gmail.com>
2022-02-12 23:55:35 +0700

    setup socket server

---
+++ server/pkg/socket/server.go
@@ -0,0 +1,81 @@
+package socket
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/tahsinature/future-proof-gin/pkg/services"
+)
+
+var dataMap = make(map[string]interface{})
+
+var (
+	telegramService = new(services.Telegram)
+	ipService       = new(services.IP)
+)
+
+func LogNewUser(ip string) {
+	if dataMap[ip] == nil {
+
+		ipData := ipService.Lookup(ip)
+		msg := fmt.Sprintf(`
+ip: %s
+city: %s
+country: %s
+org: %s
+asn: %s
+error: %t
+more1: https://freegeoip.app/json/%s
+more2: ip2location.com/%s
+`,
+			ipData.Ip,
+			ipData.City,
+			ipData.CountryName,
+			ipData.Org,
+			ipData.ASN,
+			ipData.Error,
+			ip,
+			ip)
+
+		telegramService.SendMessage(msg)
+		fmt.Println(msg)
+
+		dataMap[ip] = ipData
+
+		time.AfterFunc(time.Minute*5, func() {
+			dataMap[ip] = nil
+		})
+	}
+}
+
+func Setup(engine *gin.Engine) {
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		ip := s.RemoteAddr().String()
+		LogNewUser(ip)
+		fmt.Println("closed", reason)
+	})
+
+	engine.GET("/socket.io/", gin.WrapH(server))
+	engine.POST("/socket.io/", gin.WrapH(server))
+
+	go server.Serve()
+}
