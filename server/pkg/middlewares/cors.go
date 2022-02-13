fc8341c5175c7b80873885c241fb70e7d9ac742d
Tahsin <tahsingenuies@gmail.com>
2022-02-13 13:24:30 +0700

    use url for cors from config

--- server/pkg/middlewares/cors.go
+++ server/pkg/middlewares/cors.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tahsinature/future-proof-gin/pkg/config"
 )
 
 func Cors(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", config.App.FrontendURL)
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
