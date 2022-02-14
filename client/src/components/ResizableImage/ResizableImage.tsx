5e740685a162097754cbb5e575cfdb220232a6bc
Tahsin <tahsingenuies@gmail.com>
2022-02-14 09:03:07 +0700

    fix resizable image componenet

--- client/src/components/ResizableImage/ResizableImage.tsx
+++ client/src/components/ResizableImage/ResizableImage.tsx
@@ -1,41 +1,42 @@
 import React from 'react';
 // import Resizer from 'react-image-file-resizer';
 
-import images from 'src/assets/images';
+// import images from 'src/assets/images';
 
-const resizeFile = (file: any) =>
-  new Promise(resolve => {
-    // Resizer.imageFileResizer(
-    //   file,
-    //   300,
-    //   300,
-    //   'JPEG',
-    //   100,
-    //   0,
-    //   uri => {
-    //     resolve(uri);
-    //   },
-    //   'base64',
-    // );
-  });
+// const resizeFile = (file: any) =>
+//   new Promise(resolve => {
+//     // Resizer.imageFileResizer(
+//     //   file,
+//     //   300,
+//     //   300,
+//     //   'JPEG',
+//     //   100,
+//     //   0,
+//     //   uri => {
+//     //     resolve(uri);
+//     //   },
+//     //   'base64',
+//     // );
+//   });
 
 function ResizableImage(props: { src: string }) {
-  const reader = new FileReader();
+  // const reader = new FileReader();
 
-  reader.addEventListener('load', () => {
-    console.log('loaded');
-    // setImageToResizeUri(reader.result);
-  });
+  // reader.addEventListener('load', () => {
+  //   console.log('loaded');
+  //   // setImageToResizeUri(reader.result);
+  // });
 
-  if (!props.src.startsWith('data:image')) {
-    reader.readAsDataURL(props.src as any);
-    // URL.createObjectURL(props.src as any);
-    // resizeFile(props.src).then(console.log).catch(console.error);
-    // console.log(props.src);
-  }
+  // if (!props.src.startsWith('data:image')) {
+  //   reader.readAsDataURL(props.src as any);
+  //   // URL.createObjectURL(props.src as any);
+  //   // resizeFile(props.src).then(console.log).catch(console.error);
+  //   // console.log(props.src);
+  // }
 
   return (
-    <img src={images.gifs.iPhoneSpinner} alt="" />
+    // <img src={images.gifs.iPhoneSpinner} alt="" />
+    <img src={props.src} alt={'logo'} />
     // <div>
     // </div>
   );
