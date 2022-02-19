aee8544a11aa2666cf2ee07b21945939a9c379c8
Tahsin <tahsingenuies@gmail.com>
2022-02-20 00:10:14 +0700

    add buttons and make them interactive

--- client/src/components/HomeSections/Sections/PublishedWriting/PublishedWriting.tsx
+++ client/src/components/HomeSections/Sections/PublishedWriting/PublishedWriting.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { DescriptionRounded } from '@material-ui/icons';
 
 import Header from 'src/components/Header/Header';
@@ -7,15 +7,27 @@ import classes from './PublishedWriting.module.scss';
 import data from 'src/api/data';
 
 const PublishedWriting = () => {
+  const postsBox = useRef(null);
+
   const handleClick = (url: string) => {
     const win = window.open(url, '_blank');
     win?.focus();
   };
 
+  const goRight = () => {
+    const ele: any = postsBox.current;
+    if (ele !== null) ele.scroll(ele.scrollLeft + 100, 0);
+  };
+
+  const goLeft = () => {
+    const ele: any = postsBox.current;
+    if (ele !== null) ele.scroll(ele.scrollLeft - 100, 0);
+  };
+
   return (
     <Section classNames={[classes.PublishedWriting]}>
       <Header title="Published Writing" icon={<DescriptionRounded />} />
-      <div className={classes.Boxes}>
+      <div className={classes.Boxes} ref={postsBox}>
         {data.articles.map(writing => (
           <div key={writing.url} className={classes.Box} onClick={() => handleClick(writing.url)}>
             <div className={classes.BoxContent}>
@@ -32,6 +44,10 @@ const PublishedWriting = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className={classes.Buttons}>
+        <button onClick={goLeft}>Previous</button>
+        <button onClick={goRight}>Next</button>
       </div>
     </Section>
   );
