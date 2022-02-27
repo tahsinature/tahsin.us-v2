44a1aeeb3b5c618e0ccb813e5bd86b2cbacb814a
Tahsin <tahsingenuies@gmail.com>
2022-02-27 23:21:17 +0700

    adjust the test

--- server/pkg/tests/ping_test.go
+++ server/pkg/tests/ping_test.go
@@ -7,18 +7,15 @@ import (
 
 	"github.com/go-playground/assert/v2"
 	"github.com/tahsinature/tahsin.us/pkg/application"
-	"github.com/tahsinature/tahsin.us/pkg/db"
 	"github.com/tahsinature/tahsin.us/pkg/routes"
 )
 
 func TestPingRoute(t *testing.T) {
 	new(application.Application).Setup()
-	db.Sync(true)
 	router := routes.Setup()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping/", nil)
+	req, _ := http.NewRequest("GET", "/api/ping/", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, `{"message":"pong!"}`, w.Body.String())
 }
