92331bc7355d77d73e4caded1d3398a9cfe2e62b
Tahsin <tahsingenuies@gmail.com>
2022-02-28 13:52:38 +0700

    wrap with graphql provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import App from './App';
 import storeManager from 'src/store/storeManager';
 import * as serviceWorker from './serviceWorker';
+import config from 'src/config';
+
+const client = new ApolloClient({
+  uri: `${config.backEndBaseUrl}/graph`,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={storeManager.store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={storeManager.store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('iamassumingyouareadevelopertoo'),
 );
 
