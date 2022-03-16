f0933ee94979636ab33cfdb4914892a390b811bf
Tahsin <tahsingenuies@gmail.com>
2022-03-16 15:24:00 +0700

    make the avatar bigger

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className={classes.Home}>
       <header className="tc pv4 pv5-ns">
-        <img src={data.basicData.personal.avatar} className="br-100 pa1 ba b--black-10 h3 w3" alt="avatar" />
+        <img src={data.basicData.personal.avatar} className={['br-100 pa1 ba b--black-10', classes.Avatar].join(' ')} alt="avatar" />
         <h1 className="f5 f4-ns fw6 avenir">{data.basicData.personal.name}</h1>
         <h2 className="f6 ttu tracked">{data.basicData.personal.profession}</h2>
         <small className="tracked">{data.basicData.personal.email}</small>
