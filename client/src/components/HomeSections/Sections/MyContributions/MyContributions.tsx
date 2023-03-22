8d0c6b6a806e614457ed51da498d7efbbd553c2d
Tahsin <tahsingenuies@gmail.com>
2023-03-22 21:59:52 +0700

    fetch projects and show them through activity card

--- client/src/components/HomeSections/Sections/MyContributions/MyContributions.tsx
+++ client/src/components/HomeSections/Sections/MyContributions/MyContributions.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import { AssessmentRounded } from '@material-ui/icons';
+import { gql, useQuery } from '@apollo/client';
 
 import Header from 'src/components/Header/Header';
 import Section from 'src/components/Section/Section';
-import data from 'src/api/data';
+import oldData from 'src/api/data';
+import Cards from 'src/components/Cards/Cards';
+import GraphLoader from 'src/components/GraphLoader/GraphLoader';
+
+const GET_PROJECTS = gql`
+  query {
+    projects {
+      id
+      name
+      image
+      url
+    }
+  }
+`;
+
+interface Response {
+  projects: {
+    id: string;
+    name: string;
+    image: string;
+    url: string;
+  }[];
+}
 
 const MyContributions = () => {
+  const { loading, data, error } = useQuery<Response>(GET_PROJECTS);
+  if (!data) return null;
+
   return (
-    <Section>
-      <Header title="Projects" icon={<AssessmentRounded />} />
-      <ul>
-        {data.contribution.map(ele => (
-          <li key={ele.title}>
-            <p>
-              <a className="fw6 green no-underline underline-hover" href={ele.url} target="_blank" rel="noreferrer">
-                {ele.title}
-              </a>
-              : {ele.description}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <GraphLoader hideOnError loading={loading} data={data.projects} error={error} loadingMsg="Fetching articles from different platforms...">
+      <Section>
+        <Header title="Personal & Open Source Contributions" icon={<AssessmentRounded />} />
+        <Cards
+          data={data.projects.map(ele => ({
+            name: ele.name,
+            image: ele.image,
+            clickHandler: () => window.open(ele.url, '_blank'),
+          }))}
+        />
+      </Section>
+    </GraphLoader>
   );
 };
 
